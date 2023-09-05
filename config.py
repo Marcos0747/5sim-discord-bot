f6702c20a437dfaf7959f3e7b5804cac3e07b5fb
Marcos0747 <98708762+Marcos0747@users.noreply.github.com>
2023-09-05 11:09:46 +0200

    Update config.py

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ RPCS = ["",
 
 BOT_TOKEN = "" # https://discord.com/developers/applications/   ¡Recuerda activar los intents!
 API_TOKEN = "" # https://5sim.net/settings/security  
-BOT_STATUS = "" #idle, dnd, online
+BOT_STATUS = "" # idle, dnd, online
 MESSAGE_LOGS = True # Cambia a False si no deseas hacerlo (True o False).
 LOGGER_BUY = True # Cambia a false si no deseas loggear cada vez que alguien usa el comando buy (True o False).
 # Si lo pones en true, completa la webhook abajo 👇
